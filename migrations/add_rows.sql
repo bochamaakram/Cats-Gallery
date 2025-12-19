771b7b924cced72673e48e22963f4d572c7081a5
bochamaakram <akrambouchama0@gmail.com>
2025-12-19 15:26:06 +0100

    hh

---
+++ migrations/add_rows.sql
@@ -0,0 +1,11 @@
+INSERT INTO cats (name, pfp) VALUES
+  ('Luna', 'https://cataas.com/cat/says/luna'),
+  ('Milo', 'https://cataas.com/cat/says/milo'),
+  ('Oliver', 'https://cataas.com/cat/says/oliver'),
+  ('Leo', 'https://cataas.com/cat/says/leo'),
+  ('Bella', 'https://cataas.com/cat/says/bella'),
+  ('Charlie', 'https://cataas.com/cat/says/charlie'),
+  ('Willow', 'https://cataas.com/cat/says/willow'),
+  ('Lucy', 'https://cataas.com/cat/says/lucy'),
+  ('Simba', 'https://cataas.com/cat/says/simba'),
+  ('Max', 'https://cataas.com/cat/says/max');
